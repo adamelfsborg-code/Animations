fe1f03e94b3fc477c4492897efff9040391aead7
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-16 12:22:10 +0200

    feat: add animation pulse effect

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -1,14 +1,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap me") {
+            print("Tapped")
         }
-        .padding()
+        .padding(50)
+        .background(.indigo)
+        .foregroundColor(.white)
+        .clipShape(.circle)
+        .overlay(
+            Circle()
+                .stroke(.indigo)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount)
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
