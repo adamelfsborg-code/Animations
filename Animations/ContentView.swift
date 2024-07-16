bae79599f4b1c4cb6b0848ba9ae00235b81aa889
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-16 12:31:52 +0200

    feat: animation binding

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -3,25 +3,20 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap me") {
-            print("Tapped")
-        }
-        .padding(50)
-        .background(.indigo)
-        .foregroundColor(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.indigo)
+        print(animationAmount)
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(.bouncy), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap me") {
+                animationAmount += 1
+            }
+                .padding(20)
+                .background(.brown)
+                .foregroundColor(.white)
+                .clipShape(.capsule)
                 .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount)
-        )
-        .onAppear {
-            animationAmount = 2
         }
     }
 }
