c88d649cd445efe494ca5bda876977506e1e607a
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-16 13:07:21 +0200

    feat: drag gesture

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -1,20 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        Button("Tap me") {
-            enabled.toggle()
-        }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-        .foregroundColor(.white)
-        .animation(.default, value: enabled)
-        .clipShape(.rect(cornerRadius: enabled ? 50 : 0))
-        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation  }
+                    .onEnded { _ in
+                        withAnimation(.bouncy) {
+                            dragAmount = .zero
+                        }
+                    }
+                        
+            )
+            
     }
 }
-
 #Preview {
     ContentView()
 }
