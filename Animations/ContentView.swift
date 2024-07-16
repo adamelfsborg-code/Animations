6641635b76e1e5054d1e2d0fb260bca75863ae40
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-16 12:44:02 +0200

    feat: explicit animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -1,23 +1,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     var body: some View {
-        print(animationAmount)
-        return VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(.bouncy), in: 1...10)
-            
-            Spacer()
-            
-            Button("Tap me") {
-                animationAmount += 1
+        Button("Tap me") {
+            withAnimation {
+                animationAmount += 360
             }
-                .padding(20)
-                .background(.brown)
-                .foregroundColor(.white)
-                .clipShape(.capsule)
-                .scaleEffect(animationAmount)
         }
+        .padding(20)
+        .background(.brown)
+        .foregroundColor(.white)
+        .clipShape(.capsule)
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 0, y: 0, z: 1)
+        )
     }
 }
 
